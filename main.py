044d0620d1fb92fdaabe6afa5fa2679db1fd426f
Isabelle <isa_landini@hotmail.com>
2025-11-07 14:04:39 -0300

    Resolvendo problema de conflito de informações dentro do body da requisição

--- main.py
+++ main.py
@@ -14,7 +14,7 @@
 #Update
 #Delete
 
-#vamos caessar nosso endpoint
+#vamos acessar nosso endpoint
 #vamos acessar os PATH´s 
 
 #Path ou Rota
@@ -59,15 +59,19 @@ def post_livros(id_livro: int, livro: Livro):
 # 1- qual é o tenis -> id_livro
 #2- pegar o tenis -> pegar o livro -> id_livro
 #3- processo de pintura para mudar a cor -> atualização das informações do livro
-
+# dicionario = hasmap
+# chave - valor
 @app.put("/atualiza/{id_livro}")
-def put_livros (id_livro= int, livro= Livro):
-    meu_livro= meus_livrozinhos.get(id_livro)
+def put_livros (id_livro: int, livro: Livro):
+    meu_livro= meus_livrozinhos.get(id_livro) # aqui p meu_livro recebe o valor e vira um hashmap(dicionario)
     if not meu_livro:
         raise HTTPException(status_code=404, detail= "Esse livro não foi encontrado.")
     else:
-        meu_livro[id_livro]= livro.dict()    
-        return {"messafe": "As infirmações do seu livro foram atualizadad com sucesso!"}
+        #jogo a informação dentro do meu antigo dicionario(que é meus_livrozinhos)
+        # e NÃO  dentro da REFERENCIA do antigo dicionário
+        # antigo dicionario !=(diferente) Referencia do antigo dicionario
+        meus_livrozinhos[id_livro]= livro.dict()    # com o dict cria um novo hashmap/dicionario
+        return {"message": "As informações do seu livro foram atualizadad com sucesso!"}
 
 @app.delete("/deletar/{id_livro}")
 def delete_livro (id_livro:int):
